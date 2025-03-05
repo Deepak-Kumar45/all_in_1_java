51688a8d459a8d5217ad8fc21690c6adf34b93b5
Deepak-Kumar45 <kumardeep4591@gmail.com>
2025-03-05 22:55:51 +0530

    Import and passbyvalue added

---
+++ src/main/java/com/topics/extra/ImportAndStaticImport.java
@@ -0,0 +1,15 @@
+package com.topics.extra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImportAndStaticImport {
+	public static void main(String[] args) {
+		List<Integer> list=new ArrayList<>();
+		list.add(10);
+		
+		double sqrt = Math.sqrt(81);
+		System.err.println(sqrt);
+		
+	}
+}
--- src/main/java/com/topics/extra/PassbyValueAndReference.java
+++ src/main/java/com/topics/extra/PassbyValueAndReference.java
@@ -1,5 +1,28 @@
 package com.topics.extra;
 
+class ObjectPass{
+	int val;
+}
+
 public class PassbyValueAndReference {
 
+	private static void modifyObject(int val) {
+		val=50;
+		System.out.println("Var inside method = "+val);
+	}	
+	
+	private static void modifyObject2(ObjectPass object) {
+		object=new ObjectPass(); // reassigning the object
+		object.val=200;
+		System.out.println("Object Var inside method = "+object.val);
+	}
+	
+	public static void main(String[] args) {
+		ObjectPass objectPass=new ObjectPass();
+		objectPass.val=100;
+		modifyObject2(objectPass);
+		System.out.println("Object Var outside method = "+objectPass.val);
+		
+	}
+	
 }
