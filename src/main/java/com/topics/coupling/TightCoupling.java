f5c8fc1e182924f824b5f09a2ddb151a41c06ab6
Deepak kumar <kumardeep4591@gmail.com>
2025-03-27 17:50:16 +0530

    Functional interfaces

--- src/main/java/com/topics/coupling/TightCoupling.java
+++ src/main/java/com/topics/coupling/TightCoupling.java
@@ -1,50 +1,50 @@
-package com.topics.coupling;
+// package com.topics.coupling;
 
-class Engine {
-    public void startEngine() {
-        System.out.println("!!Engine is starting!!");
-    }
-}
+// class Engine {
+// public void startEngine() {
+// System.out.println("!!Engine is starting!!");
+// }
+// }
 
-class PetrolEngine {
-    public void startPetrolEngine() {
-        System.out.println("!!Petrol Engine is starting!!");
-    }
-}
+// class PetrolEngine {
+// public void startPetrolEngine() {
+// System.out.println("!!Petrol Engine is starting!!");
+// }
+// }
 
-class ElectricEngine {
-    public void startElectricEngine() {
-        System.out.println("!!Electric Engine is starting!!");
-    }
-}
+// class ElectricEngine {
+// public void startElectricEngine() {
+// System.out.println("!!Electric Engine is starting!!");
+// }
+// }
 
-class Car {
+// class Car {
 
-    // private Engine engine;
-    // private PetrolEngine petrolEngine;
-    // private ElectricEngine electricEngine;
+// private Engine engine;
+// private PetrolEngine petrolEngine;
+// private ElectricEngine electricEngine;
 
-    // public Car() {
-    // engine = new Engine(); // Engine Object is directly created
-    // petrolEngine = new PetrolEngine();
-    // electricEngine = new ElectricEngine();
-    // }
+// public Car() {
+// engine = new Engine(); // Engine Object is directly created
+// petrolEngine = new PetrolEngine();
+// electricEngine = new ElectricEngine();
+// }
 
-    // public void startCar() {
-    // // engine.startEngine();
-    // petrolEngine.startPetrolEngine();
-    // // electricEngine.startElectricEngine();
-    // System.out.println("Car is moving forward |->|->|->|->|->");
-    // }
+// public void startCar() {
+// // engine.startEngine();
+// petrolEngine.startPetrolEngine();
+// // electricEngine.startElectricEngine();
+// System.out.println("Car is moving forward |->|->|->|->|->");
+// }
 
-}
+// }
 
-public class TightCoupling {
-    public static void main(String[] args) {
-        System.out.println();
-        Car car = new Car();
-        // car.startCar();
-        System.out.println();
+// public class TightCoupling {
+// public static void main(String[] args) {
+// System.out.println();
+// Car car = new Car();
+// car.startCar();
+// System.out.println();
 
-    }
-}
+// }
+// }
